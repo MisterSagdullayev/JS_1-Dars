b02f71d4af27128b2b53e64ab43444d45a582491
Jasur Sagdullayev <168184569+MisterSagdullayev@users.noreply.github.com>
2024-05-06 17:00:09 +0500

    JS 1-Dars
    Zeromax IT-Center

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*console.log("Men faqat konsol oynasida ko'rinaman!");
+alert("Saytomga hush kelibsiz!");
+document.write("200+200*5-100");*/
+
+/*let x = 5;
+let z = 7;
+x = 10;*/
+/*let salom = "Assalomu alaykum!"*/
+/*let x = 10, z = 7, y= 25*/
+/*console.log(salom);*/
+
+/* Data types in JS */
+/*let soz = "JavaScript";         /!* String *!/
+let son = 5;        /!* Number *!/
+document.write(`son);*/
+
+/* Math in JS */
+
+let x = 5;
+/*x +=5*/
+/*x -= 2;*/
+/*x *= 5;*/
+/*x /= 2;*/
+/*let natija = x**5;*/
+/*++x;*/
+/*--x;
+--x;*/
+x++;
+x++;
+x++;
+
+
+document.write(x)
+
